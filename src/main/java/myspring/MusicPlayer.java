5ec2453bf924a9fee2640a91d7a83edc4e3811d0
Marat-Raz <maru-rmr@yandex.ru>
2025-11-12 22:04:49 +0300

    first method

--- src/main/java/myspring/MusicPlayer.java
+++ src/main/java/myspring/MusicPlayer.java
@@ -4,7 +4,13 @@ public class MusicPlayer {
 
   private Music music;
 
+  public MusicPlayer() {}
+
   public MusicPlayer(Music music) {
+    this.music = music;
+  }
+
+  public void setMusic(Music music) {
     this.music = music;
   }
 
