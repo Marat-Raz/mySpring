cc33153a22ba0b3c59927209000c527ea84aae8f
Marat-Raz <maru-rmr@yandex.ru>
2025-11-19 22:16:54 +0300

    lesson12

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,3 +6,17 @@ O
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+U
+%src/main/java/myspring/RockMusic.java,8\c\8cbe879f3a014a847ff9c1f046d0c648a6bb8d63
+Q
+!src/main/java/myspring/Music.java,9\4\94b89d23fe02faa216d93145fe50aeaadee5194d
+T
+$src/main/java/myspring/Computer.java,7\f\7fde0ddb263dce42745d757fb500090d40e9972f
+V
+&src/main/java/myspring/RelaxMusic.java,4\3\43083546f6380f65578d67a760e1ab3a0eaf33b3
+Y
+)src/main/resources/musicPlayer.properties,e\a\ea1468302047bdc3406502196822e83e7676fc35
--- src/main/java/myspring/TestSpring.java
+++ src/main/java/myspring/TestSpring.java
@@ -8,17 +8,14 @@ public class TestSpring {
     ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
         "applicationContext.xml");
 
-/*    Music music = context.getBean("classicalMusic", Music.class);
-    MusicPlayer musicPlayer = new MusicPlayer(music);
-    musicPlayer.playMusic();
+    MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+    System.out.println(musicPlayer.getName());
+    System.out.println(musicPlayer.getVolume());
 
-    Music music1 = context.getBean("rockMusic", Music.class);
-    MusicPlayer rockPlayer = new MusicPlayer(music1);
-    rockPlayer.playMusic();*/
-/*    MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-    musicPlayer.playMusic();*/
-    Computer computer = context.getBean("computer", Computer.class);
-    System.out.println(computer);
+    ClassicalMusic musicPlayer1 = context.getBean("classicalMusic", ClassicalMusic.class);
+
+
+
     context.close();
 
   }
